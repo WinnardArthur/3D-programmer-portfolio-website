e203e796e1598e21d3a129def5637452130ea058
Winnard Arthur <winnardarthur7@gmail.com>
2023-09-18 09:38:46 +0000

    setup base structure

---
+++ src/utils/motion.js
@@ -0,0 +1,88 @@
+// Text variant animation
+export const textVariant = (delay) => {
+  return {
+    hidden: {
+      y: -50,
+      opacity: 0,
+    },
+    show: {
+      y: 0,
+      opacity: 1,
+      transition: {
+        type: "spring",
+        duration: 1.25,
+        delay: delay,
+      },
+    },
+  };
+};
+
+// Fade in animation
+export const fadeIn = (direction, type, delay, duration) => {
+  return {
+    hidden: {
+      x: direction === "left" ? 100 : direction === "right" ? -100 : 0,
+      y: direction === "up" ? 100 : direction === "down" ? -100 : 0,
+      opacity: 0,
+    },
+    show: {
+      x: 0,
+      y: 0,
+      opacity: 1,
+      transition: {
+        type,
+        delay,
+        duration,
+      },
+    },
+  };
+};
+
+// Zoom in animation
+export const zoomIn = (delay, duration) => {
+  return {
+    hidden: {
+      scale: 0,
+      opacity: 0,
+    },
+    show: {
+      scale: 1,
+      opacity: 1,
+      transition: {
+        type: "tween",
+        ease: "easeOut",
+        delay,
+        duration,
+      },
+    },
+  };
+};
+
+// Slide in animation
+export const slideIn = (direction, type, delay, duration) => {
+  return {
+    hidden: {
+      x: direction === "left" ? "-100%" : direction === "right" ? "100%" : 0,
+      y: direction === "up" ? "-100%" : direction === "down" ? "100%" : 0,
+    },
+    show: {
+      type,
+      delay,
+      duration,
+      ease: "easeOut",
+    },
+  };
+};
+
+// Stagger Children
+export const staggerContainer = (staggerChildren, delayChildren) => {
+  return {
+    hidden: {},
+    show: {
+      transition: {
+        staggerChildren,
+        delayChildren: delayChildren || 0,
+      },
+    },
+  };
+};
